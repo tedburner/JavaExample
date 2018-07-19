a9ed25cd472d4a565011e3a6c28aa641c40df0c8
lingjun.jlj <jjl@33.cn>
2018-07-19 11:52:10 +0800

    过滤模式

---
+++ src/main/java/DesignPatterns/filter/CriteriaMale.java
@@ -0,0 +1,22 @@
+package DesignPatterns.filter;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author: lingjun.jlj
+ * @Date: 2018/7/19 11:34
+ * @Description: 接口实体类
+ */
+public class CriteriaMale implements Criteria {
+
+    @Override
+    public List<Person> meetCriteria(List<Person> personList) {
+        List<Person> malePersons = personList.stream()
+                .filter(x -> x.getGender().equalsIgnoreCase("MALE"))
+                .collect(Collectors.toList());
+        return malePersons;
+    }
+}
