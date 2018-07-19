be5df29d57e714baaba5bbda7123652201c26d70
lingjun.jlj <jjl@33.cn>
2018-07-19 21:26:59 +0800

    解释器

---
+++ src/main/java/DesignPatterns/InterpreterPattern/TerminalExpression.java
@@ -0,0 +1,22 @@
+package DesignPatterns.InterpreterPattern;
+
+/**
+ * @author: lingjun.jlj
+ * @Date: 2018/7/19 21:23
+ * @Description:
+ */
+public class TerminalExpression implements Expression {
+    private String data;
+
+    public TerminalExpression(String data){
+        this.data = data;
+    }
+
+    @Override
+    public Boolean interpret(String context) {
+        if(context.contains(data)){
+            return true;
+        }
+        return false;
+    }
+}
