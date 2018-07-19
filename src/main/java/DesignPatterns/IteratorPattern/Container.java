2f94857749f9ef166339e7c44588674c3ef4eef7
lingjun.jlj <jjl@33.cn>
2018-07-19 21:43:05 +0800

    fix 中介者模式

---
+++ src/main/java/DesignPatterns/IteratorPattern/Container.java
@@ -0,0 +1,10 @@
+package DesignPatterns.IteratorPattern;
+
+/**
+ * @author: lingjun.jlj
+ * @Date: 2018/7/19 21:29
+ * @Description:
+ */
+public interface Container {
+    Iterator getIterator();
+}
