66f7a11023a47e72135cd5a03458ee74d9c0f36f
lingjun.jlj <jjl@33.cn>
2018-08-07 22:24:07 +0800

    fix

---
+++ src/main/java/algorithm/acm/CardSort.java
@@ -0,0 +1,21 @@
+package algorithm.acm;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2018/8/7 15:37
+ * @description:
+ */
+public class CardSort {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i <= 5; i++) {
+            list.add(i);
+        }
+
+    }
+}
---
+++ src/main/java/basejava/basics/concurrent/lock/ReentrantReadWriteLockTest.java
@@ -0,0 +1,21 @@
+package basejava.basics.concurrent.lock;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2018/8/7 17:01
+ * @description:
+ */
+public class ReentrantReadWriteLockTest {
+
+    public static void main(String[] args) {
+        ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+        //读锁
+        lock.readLock().lock();
+        lock.readLock().unlock();
+        //写锁
+        lock.writeLock().lock();
+        lock.writeLock().unlock();
+    }
+}
