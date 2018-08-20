87104c39ee5ea36d21ad92df068c6f8962fa0771
lingjun.jlj <jjl@33.cn>
2018-08-20 13:58:34 +0800

    fix

--- src/main/java/com/example/basejava/basics/map/ConcurrentHashMapTest.java
+++ src/main/java/com/example/basejava/basics/map/ConcurrentHashMapTest.java
@@ -12,9 +12,9 @@ public class ConcurrentHashMapTest {
 
     public static void main(String[] args) {
 
-        Map<Integer,String> map = new ConcurrentHashMap<>();
+        Map<Integer, String> map = new ConcurrentHashMap<>();
 
-        map.put(1,"1");
-        map.put(2,"2");
+        map.put(1, "1");
+        map.put(2, "2");
     }
 }
