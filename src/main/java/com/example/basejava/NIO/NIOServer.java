b5c7ad5d127f538f4c04394b0533fa3298fa462a
jiang95 <jlj_vip98@qq.com>
2018-10-08 18:02:33 +0800

    netty demo

---
+++ src/main/java/com/example/basejava/NIO/NIOServer.java
@@ -0,0 +1,94 @@
+package com.example.basejava.NIO;
+
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2018/10/8 17:06
+ * @description:
+ */
+public class NIOServer {
+
+    public static void main(String[] args) throws Exception {
+
+        Selector serverSelector = Selector.open();
+        Selector clientSelector = Selector.open();
+
+        new Thread(() -> {
+            try {
+                // 对应IO编程中服务端启动
+                ServerSocketChannel listenerChannel = ServerSocketChannel.open();
+                listenerChannel.socket().bind(new InetSocketAddress(8100));
+                listenerChannel.configureBlocking(false);
+                listenerChannel.register(serverSelector, SelectionKey.OP_ACCEPT);
+
+                while (true) {
+                    //监听是否有新的连接，这里的1指的是阻塞时间为1ms
+                    Set<SelectionKey> set = serverSelector.selectedKeys();
+                    Iterator<SelectionKey> keyIterator = set.iterator();
+
+                    while (keyIterator.hasNext()) {
+                        SelectionKey key = keyIterator.next();
+
+                        if (key.isAcceptable()) {
+                            try {
+                                //每来一个新连接，不需要创建一个线程，而是直接注册到clientSelector中
+                                SocketChannel clientChannel = ((ServerSocketChannel) key.channel()).accept();
+                                clientChannel.configureBlocking(false);
+                                clientChannel.register(clientSelector, SelectionKey.OP_ACCEPT);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            } finally {
+                                keyIterator.remove();
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                while (true) {
+                    // (2) 批量轮询是否有哪些连接有数据可读，这里的1指的是阻塞的时间为 1ms
+                    if (clientSelector.select(1) > 0) {
+                        Set<SelectionKey> set = clientSelector.selectedKeys();
+                        Iterator<SelectionKey> keyIterator = set.iterator();
+
+                        while (keyIterator.hasNext()) {
+                            SelectionKey key = keyIterator.next();
+
+                            if (key.isReadable()) {
+                                try {
+                                    SocketChannel clientChannel = (SocketChannel) key.channel();
+                                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                                    // (3) 读取数据以块为单位批量读取
+                                    clientChannel.read(byteBuffer);
+                                    byteBuffer.flip();
+                                    System.out.println(Charset.defaultCharset().newDecoder().decode(byteBuffer));
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+
+    }
+}
---
+++ src/main/java/com/example/netty/test/NettyClient.java
@@ -0,0 +1,42 @@
+package com.example.netty.test;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2018/10/8 17:43
+ * @description:
+ */
+public class NettyClient {
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Bootstrap bootstrap = new Bootstrap();
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel channel) throws Exception {
+                        channel.pipeline().addLast(new StringEncoder());
+                    }
+                });
+
+        Channel channel = bootstrap.connect("127.0.0.1", 8200).channel();
+        while (true) {
+            channel.writeAndFlush(sdf.format(new Date()) + " : Hello World!");
+            Thread.sleep(2000);
+        }
+    }
+}
