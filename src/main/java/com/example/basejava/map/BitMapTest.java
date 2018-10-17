86b30d5158e7f69c9bff9ef4196c7f4c8ff6e4a5
jiang95 <jlj_vip98@qq.com>
2018-10-17 17:09:37 +0800

    fix bitmap

---
+++ src/main/java/com/example/basejava/map/BitMapTest.java
@@ -0,0 +1,15 @@
+package com.example.basejava.map;
+
+import sun.jvm.hotspot.utilities.BitMap;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2018/10/17 17:06
+ * @description:
+ */
+public class BitMapTest {
+
+    public static void main(String[] args) {
+        BitMap bitMap = new BitMap(100);
+    }
+}
