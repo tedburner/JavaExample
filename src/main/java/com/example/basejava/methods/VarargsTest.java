2424f737f7544bf64b0bb55c7c77fd52f9e90347
Lucifer <jlj_vip98@qq.com>
2018-11-21 14:24:43 +0800

    fix

---
+++ src/main/java/com/example/basejava/methods/VarargsTest.java
@@ -0,0 +1,23 @@
+package com.example.basejava.methods;
+
+/**
+ * @author: Lucifer
+ * @date: 2018/11/19 15:00
+ * @description:
+ */
+public class VarargsTest {
+
+    public static void main(String[] args) {
+        int count = sum(1, 2, 3, 4);
+        System.out.println(count);
+
+    }
+
+    public static int sum(int... args) {
+        int count = 0;
+        for (int arg : args) {
+            count += arg;
+        }
+        return count;
+    }
+}
