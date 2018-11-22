1508e7cd3bf00e8ee661b623229b4add44e8f714
Lucifer <jlj_vip98@qq.com>
2018-11-22 19:18:32 +0800

    fix

--- src/main/java/com/example/test/Test.java
+++ src/main/java/com/example/test/Test.java
@@ -8,18 +8,20 @@ package com.example.test;
 public class Test {
 
     public static void main(String[] args) {
-        Object[] elementData  =new Object[10];
-        int elementCount = 2;
-        int index = 1;
-        Object obj = "vector";
-        Object obj1 = "vector1";
+//        Object[] elementData  =new Object[10];
+//        int elementCount = 2;
+//        int index = 1;
+//        Object obj = "vector";
+//        Object obj1 = "vector1";
+//
+//        elementData[0] = obj;
+//        elementData[1] = obj;
+//        elementData[2] = obj1;
+//        elementData[3] = obj;
+//        System.arraycopy(elementData, index, elementData, index + 1, elementCount - index);
+//        System.out.print(elementData.toString());
 
-        elementData[0] = obj;
-        elementData[1] = obj;
-        elementData[2] = obj1;
-        elementData[3] = obj;
-        System.arraycopy(elementData, index, elementData, index + 1, elementCount - index);
-        System.out.print(elementData.toString());
+        System.out.println(System.currentTimeMillis());
 
 
     }
