d422260578b54d8537773c5200a85c0fde9a8553
Lucifer <jlj_vip98@qq.com>
2018-11-28 16:39:50 +0800

    运算符操作

---
+++ src/main/java/com/example/basejava/OperationalCharacterTest.java
@@ -0,0 +1,29 @@
+package com.example.basejava;
+
+/**
+ * @author: Lucifer
+ * @date: 2018-11-28 16:33
+ * @description: 运算符
+ */
+public class OperationalCharacterTest {
+
+    public static void main(String[] args) {
+        // << 左移一位，相当于乘2
+        System.out.println(4 << 1);
+
+        // >> 右移一位，相当于除以2
+        System.out.println(4 >> 1);
+
+        // >>> 无符号右移
+        System.out.println(4 >>> 1);
+
+        // |= 类似于 += 这样的写法
+        boolean flag = true;
+        flag |= true;
+        System.out.println(flag);
+
+        // &=
+        flag &= false;
+        System.out.println(flag);
+    }
+}
--- src/main/java/com/example/test/Test.java
+++ src/main/java/com/example/test/Test.java
@@ -24,5 +24,18 @@ public class Test {
         System.out.println(System.currentTimeMillis());
 
 
+        int n = 9 - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+
+        System.out.println(n);
+
+
+
+
+
     }
 }
