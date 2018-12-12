e1390230c60362a1038bc307b42738e45c714315
Lucifer <jlj_vip98@qq.com>
2018-12-12 23:15:22 +0800

    guava cache

---
+++ src/main/java/com/example/algorithm/datastructure/DataStructure.java
@@ -0,0 +1,13 @@
+package com.example.algorithm.datastructure;
+
+/**
+ * @author: Lucifer
+ * @date: 2018-12-12 19:01
+ * @description: 数据结构
+ */
+public class DataStructure {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/example/cache/Cache.java
@@ -0,0 +1,9 @@
+package com.example.cache;
+
+/**
+ * @author: Lucifer
+ * @date: 2018-12-12 22:22
+ * @description:
+ */
+public class Cache {
+}
