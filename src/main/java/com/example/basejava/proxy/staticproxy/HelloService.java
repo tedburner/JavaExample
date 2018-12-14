1186a84d3a6d28e3a45fe6c52ae0fc9c6e74d6c6
Lucifer <jlj_vip98@qq.com>
2018-12-14 10:54:45 +0800

    静态代理

---
+++ src/main/java/com/example/basejava/proxy/staticproxy/HelloService.java
@@ -0,0 +1,13 @@
+package com.example.basejava.proxy.staticproxy;
+
+/**
+ * @author: Lucifer
+ * @date: 2018-12-14 09:58
+ * @description:
+ */
+public interface HelloService {
+
+    String sayHello(String name);
+
+    String hi(String msg);
+}
---
+++ src/main/java/com/example/basejava/proxy/staticproxy/HelloServiceProxy.java
@@ -0,0 +1,33 @@
+package com.example.basejava.proxy.staticproxy;
+
+/**
+ * @author: Lucifer
+ * @date: 2018-12-14 10:00
+ * @description: 静态代理
+ */
+public class HelloServiceProxy implements HelloService {
+
+    private HelloService helloService;
+
+    public HelloServiceProxy(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @Override
+    public String sayHello(String name) {
+        System.out.println("预处理....");
+        String result = helloService.sayHello(name);
+        System.out.println(result);
+        System.out.println("后处理....");
+        return result;
+    }
+
+    @Override
+    public String hi(String msg) {
+        System.out.println("预处理...");
+        String result = helloService.hi(msg);
+        System.out.println(result);
+        System.out.println("后处理...");
+        return result;
+    }
+}
