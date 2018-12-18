7a54223cb187fabb5ee6b80c364f3cb77e6fb86e
Lucifer <jlj_vip98@qq.com>
2018-12-18 17:20:50 +0800

    disruptor 高性能队列

---
+++ src/main/java/com/example/basejava/PolymorphicCase.java
@@ -0,0 +1,65 @@
+package com.example.basejava;
+
+/**
+ * @author: Lucifer
+ * @date: 2018-12-14 11:25
+ * @description: 多态例子
+ */
+public class PolymorphicCase {
+
+    public static void main(String[] args) {
+
+        show(new Cat());
+        show(new Dog());
+
+        //向上转型
+        Animal animal = new Cat();
+        animal.eat();//调用的是Cat的 eat
+        //当使用多态方式调用方法时，首先检查父类中是否有该方法，
+        // 如果没有，则编译错误；如果有，再去调用子类的同名方法
+        //animal.work();//编译报错
+        //向下转型
+        Cat cat = (Cat) animal;
+        cat.work();//调用的是Cat的work
+    }
+
+    public static void show(Animal animal) {
+        animal.eat();
+        if (animal instanceof Cat) {
+            Cat cat = (Cat) animal;
+            cat.work();
+        }
+        if (animal instanceof Dog) {
+            Dog dog = (Dog) animal;
+            dog.work();
+        }
+    }
+}
+
+abstract class Animal {
+    abstract void eat();
+}
+
+class Cat extends Animal {
+
+    @Override
+    void eat() {
+        System.out.println("猫吃鱼....");
+    }
+
+    public void work() {
+        System.out.println("猫抓老鼠....");
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    void eat() {
+        System.out.println("狗吃骨头....");
+    }
+
+    public void work() {
+        System.out.println("look door....");
+    }
+}
---
+++ src/main/java/com/example/disruptor/DisruptorTest.java
@@ -0,0 +1,89 @@
+package com.example.disruptor;
+
+import com.lmax.disruptor.BlockingWaitStrategy;
+import com.lmax.disruptor.EventFactory;
+import com.lmax.disruptor.EventHandler;
+import com.lmax.disruptor.RingBuffer;
+import com.lmax.disruptor.dsl.Disruptor;
+import com.lmax.disruptor.dsl.ProducerType;
+
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * @author: Lucifer
+ * @date: 2018-12-18 16:53
+ * @description: Disruptor 高性能队列
+ */
+public class DisruptorTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        //队列中的元素
+        class Element {
+            private int value;
+
+            public int getValue() {
+                return value;
+            }
+
+            public void setValue(int value) {
+                this.value = value;
+            }
+        }
+
+        //生产者的线程工厂
+        ThreadFactory threadFactory = new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread(r, "simple thread");
+            }
+        };
+        //RingBuffer 生成工厂，初始化 RingBuffer的时候使用
+        EventFactory<Element> factory = new EventFactory<Element>() {
+            @Override
+            public Element newInstance() {
+                return new Element();
+            }
+        };
+
+        //处理 Event 的handler
+        EventHandler<Element> handler = new EventHandler<Element>() {
+            @Override
+            public void onEvent(Element element, long l, boolean b) throws Exception {
+                System.out.println("Element: " + element.getValue());
+            }
+        };
+
+        //阻塞策略
+        BlockingWaitStrategy strategy = new BlockingWaitStrategy();
+
+        //指定 RingBuffer
+        int bufferSize = 16;
+
+        //创建 disruptor,采用单线程模式
+        Disruptor<Element> disruptor = new Disruptor(factory, bufferSize, threadFactory, ProducerType.SINGLE, strategy);
+
+        //设置EventHandler
+        disruptor.handleEventsWith(handler);
+
+        //启动disruptor
+        disruptor.start();
+
+        RingBuffer<Element> ringBuffer = disruptor.getRingBuffer();
+
+        for (int i = 0; true; i++) {
+            //获取下一个可用位置下标
+            long sequence = ringBuffer.next();
+            try {
+                //返回可用位置的元素
+                Element event = ringBuffer.get(sequence);
+                //设置该位置元素的值
+                event.setValue(i);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                ringBuffer.publish(sequence);
+            }
+            Thread.sleep(10L);
+        }
+    }
+}
