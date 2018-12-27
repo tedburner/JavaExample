aca6962065550e3e90d9e5144f702e5813204d72
Arthas <jlj_vip98@qq.com>
2018-12-27 14:03:31 +0800

    并发工具

--- src/main/java/com/example/common/concurrent/lock/CountDownLatchTest.java
+++ src/main/java/com/example/common/concurrent/lock/CountDownLatchTest.java
@@ -1,5 +1,7 @@
 package com.example.common.concurrent.lock;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -8,11 +10,36 @@ import java.util.concurrent.CountDownLatch;
  * @description:
  */
 public class CountDownLatchTest {
+    private static CountDownLatch countDownLatch = new CountDownLatch(5);
+
+    static class BossThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Boss在会议室等待，总共有" + countDownLatch.getCount() + "人在开会");
+            try {
+                //Boss等待
+                countDownLatch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("所有人都已经到齐了，开会吧....");
+        }
+    }
+
+    static class EmployeeThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + ", 到达会议室....");
+            //员工到达会议室
+            countDownLatch.countDown();
+        }
+    }
 
     public static void main(String[] args) {
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-        //countDownLatch.await();
-        //countDownLatch.await(1L, TimeUnit.MINUTES);
-        countDownLatch.countDown();
+        new BossThread().start();
+        for (int i = 0; i < countDownLatch.getCount(); i++) {
+            System.out.println(countDownLatch.getCount());
+            new EmployeeThread().start();
+        }
     }
 }
--- src/main/java/com/example/common/concurrent/lock/CyclicBarrierTest.java
+++ src/main/java/com/example/common/concurrent/lock/CyclicBarrierTest.java
@@ -9,9 +9,31 @@ import java.util.concurrent.CyclicBarrier;
  */
 public class CyclicBarrierTest {
 
+    private static CyclicBarrier cyclicBarrier;
+
+    static class CyclicBarrierThread extends Thread {
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + "到了");
+            //等待
+            try {
+                System.out.println("有多少人在等待" + cyclicBarrier.getNumberWaiting());
+                cyclicBarrier.await();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(1);
-        //cyclicBarrier.await();
-        cyclicBarrier.reset();
+        cyclicBarrier = new CyclicBarrier(5, new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("人到齐了，开会吧....");
+            }
+        });
+
+        for (int i = 0; i < 5; i++) {
+            new CyclicBarrierThread().start();
+        }
     }
 }
