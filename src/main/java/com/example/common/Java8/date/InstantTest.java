a1f1e7d38cbb8b10de3ffc94b4598a2a7f1257ad
Arthas <jlj_vip98@qq.com>
2019-01-07 16:38:40 +0800

    fix

--- src/main/java/com/example/common/Java8/date/InstantTest.java
+++ src/main/java/com/example/common/Java8/date/InstantTest.java
@@ -2,24 +2,30 @@ package com.example.common.Java8.date;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 /**
  * @author lingjun.jlj
  * @data 2018/5/14
- * @Description:
+ * @Description: Instant 代替 Date
  */
 public class InstantTest {
 
     public static void main(String[] args) {
         Instant instant = Instant.now();
         System.out.println(instant);
-        //将java.util.Date转换为Instant
-        Instant time = Instant.ofEpochMilli(new Date().getTime());
+        //将时间戳转换为Instant
+        Instant time = Instant.ofEpochMilli(System.currentTimeMillis());
+        System.out.println(time);
+
         //从字符串类型中创建Instant类型的时间
         instant = Instant.parse("1995-10-23T10:12:35Z");
         //将现在的时间加上5个小时4分钟
         instant.plus(Duration.ofHours(5).plusMinutes(4));
+
+        System.out.println(instant);
+
 
     }
 }
