c30273c04073ecd0f95150318b3a53d3f674cff4
arthas <jlj_vip98@qq.com>
2019-01-21 10:29:31 +0800

    fix

---
+++ src/main/java/com/example/common/nio/mmap/MMapTest.java
@@ -0,0 +1,13 @@
+package com.example.common.nio.mmap;
+
+/**
+ * @author: Arthas
+ * @date: 2019-01-18 16:47
+ * @description: mmap 测试类
+ */
+public class MMapTest {
+
+    public static void main(String[] args) {
+
+    }
+}
