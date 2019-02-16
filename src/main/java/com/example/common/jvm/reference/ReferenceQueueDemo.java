101d153bbf4915c662089fc56419b96d146bdac8
arthas <jlj_vip98@qq.com>
2019-02-16 23:31:47 +0800

    引用队列

---
+++ src/main/java/com/example/common/jvm/reference/ReferenceQueueDemo.java
@@ -0,0 +1,45 @@
+package com.example.common.jvm.reference;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: Arthas
+ * @date: 2019-02-16 16:14
+ * @description: 引用队列
+ */
+public class ReferenceQueueDemo {
+
+    private static ReferenceQueue<byte[]> referenceQueue = new ReferenceQueue<>();
+    private static int _1M = 1024 * 1024;
+
+    public static void main(String[] args) {
+
+        Object object = new Object();
+        Map<Object, Object> map = new HashMap<>();
+
+        Thread thread = new Thread(() -> {
+            try {
+                int cnt = 0;
+                WeakReference<byte[]> k;
+                while ((k = (WeakReference) referenceQueue.remove()) != null) {
+                    System.out.println((cnt++) + "回收了:" + k);
+                }
+            } catch (InterruptedException e) {
+                //结束循环
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
+
+        for (int i = 0; i < 10000; i++) {
+            byte[] bytes = new byte[_1M];
+            WeakReference<byte[]> weakReference = new WeakReference<>(bytes, referenceQueue);
+            map.put(weakReference, object);
+        }
+        System.out.println("map.size->" + map.size());
+
+    }
+}
