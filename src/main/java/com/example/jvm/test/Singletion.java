733008a2a787e623e66e66cbeaa752e7a43564d5
arthas <jianglingjun@yuuwei.cn>
2019-03-31 19:12:19 +0800

    JVM

---
+++ src/main/java/com/example/jvm/test/Singletion.java
@@ -0,0 +1,31 @@
+package com.example.jvm.test;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2019/3/31 0031 18:23
+ * @description:
+ */
+public class Singletion {
+
+    private Singletion(){
+
+    }
+
+    private static class LazyHolder{
+        static final Singletion INSTANCE = new Singletion();
+        static {
+            System.out.println("LazyHolder.<clinit>");
+        }
+    }
+    public static Object getInstance(boolean flag){
+        if (flag) return new LazyHolder[2];
+        return LazyHolder.INSTANCE;
+    }
+
+    public static void main(String[] args) {
+        getInstance(true);
+        System.out.println("----------------");
+        getInstance(false);
+
+    }
+}
