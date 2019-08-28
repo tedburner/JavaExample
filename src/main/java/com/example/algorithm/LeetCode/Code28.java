7595f499efa0558c7b33f285501ff6dcf3839d21
tedburner <jlj_vip98@qq.com>
2019-08-28 23:33:59 +0800

    LeetCode

---
+++ src/main/java/com/example/algorithm/LeetCode/Code28.java
@@ -0,0 +1,34 @@
+package com.example.algorithm.LeetCode;
+
+/**
+ * @author: Arthas
+ * @date: 2019/8/28 23:13
+ * @description: 实现 strStr()
+ */
+public class Code28 {
+
+    public static void main(String[] args) {
+        System.out.println(strStr("hello", "ew"));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        if (needle == null || needle.isEmpty()) {
+            return 0;
+        }
+        int len = haystack.length();
+        int n = needle.length();
+        if (len < n) {
+            System.out.println("前者小于后者");
+            return -1;
+        }
+        for (int i = 0; i <=(len - n); i++) {
+            String str = haystack.substring(i, i + n);
+            System.out.println(str);
+            if (str.equals(needle)) {
+                System.out.println(i);
+                return i;
+            }
+        }
+        return -1;
+    }
+}
