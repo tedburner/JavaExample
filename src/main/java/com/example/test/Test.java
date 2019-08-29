0ad75d0e20ac8d86835fbc5bfd117c8c406a62ea
tedburner <jlj_vip98@qq.com>
2019-08-29 09:12:01 +0800

    fix test

--- src/main/java/com/example/test/Test.java
+++ src/main/java/com/example/test/Test.java
@@ -1,7 +1,11 @@
 package com.example.test;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Map;
 
 /**
@@ -21,145 +25,54 @@ public class Test {
         }
     }
 
-    public static void main(String[] args) {
-        String aa = "length: 10";
-        String bb = "length: " + "10";
-        String cc = "length: " + aa.length();
-        System.out.println(aa == bb);
-        System.out.println(aa == cc);
-//        List<SimpleDTO> list = new ArrayList<>();
-//        list.add(new SimpleDTO(1, "1"));
-//        list.add(new SimpleDTO(2, null));
-//        for (SimpleDTO dto : list) {
-//            Integer num = Integer.valueOf(dto.name);
-//            System.out.println(num);
-//        }
+    /**
+     * 根据地址获得数据的字节流
+     *
+     * @param strUrl 网络连接地址
+     * @return
+     */
+    public static byte[] getImageFromNetByUrl(String strUrl) {
+        try {
+            URL url = new URL(strUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setConnectTimeout(5 * 1000);
+            InputStream inStream = conn.getInputStream();//通过输入流获取图片数据
+            byte[] btImg = readInputStream(inStream);//得到图片的二进制数据
+            return btImg;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-//        String text = "请问您是#姓名#本人吗#fix#？";
-//        String[] fieldArray = StringUtils.substringsBetween(text, "#", "#");
-//        for (String field : fieldArray) {
-//            text = StringUtils.replace(text, "#" + field + "#", "蒋灵俊");
-//        }
-//
-//        System.out.println(text);
+    /**
+     * 从输入流中获取数据
+     *
+     * @param inStream 输入流
+     * @return
+     * @throws Exception
+     */
+    public static byte[] readInputStream(InputStream inStream) throws Exception {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        while ((len = inStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, len);
+        }
+        inStream.close();
+        return outStream.toByteArray();
+    }
 
-//        SimpleDTO dto = new SimpleDTO();
-//        dto.setId(1);
-//        dto.setContent("你好");
-//        dto.setName("麻辉文");
-//        String json = JSON.toJSONString(dto);
-//        System.out.println(json);
+    public static void main(String[] args) throws IOException {
+        byte[] btImg = getImageFromNetByUrl("http://vodqnuhbilj.vod.126.net/vodqnuhbilj/00f39e83-b59c-4931-99ce-7b2047750df7.mp4");
+        String md5 = DigestUtils.md2Hex(btImg);
+        System.out.println(md5 + " " + StringUtils.equals("60358b67e7ce6f31b493cbf956548909", md5));
 
-//        //Arrays.asList体现的是适配器模式，只是转换接口，后台的数据仍是数组。
-//        String[] str = new String[]{"alibaba", "腾讯"};
-//        List list = Arrays.asList(str);
-//        str[0] = "网易";
-////        list.remove("腾讯");
-//
-//        System.out.println(list);
-//        String time = "1560756159573";
-//        Date date = new Date(Long.valueOf(time));
-//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//        System.out.println(sdf.format(date));
-
-//        Map<String, Object> params = new HashMap<>();
-//        fillPdfFields(params, "income", null);
-//        System.out.println(params);
-
-//        Map<String, String> map = new HashMap<>();
-//        System.out.println(map == null);
-//        System.out.println(map.isEmpty());
-//
-//        String fileName = "/" + System.currentTimeMillis() + ".png";
-//        String imagePath = new DateTime().toString("/YYYY/MM/dd");
-//        String path = "C:\\Document\\card" + imagePath;
-//        new File(path).mkdirs();
-
-//        Integer a = null;
-//        System.out.println(String.valueOf(a));
-//        System.out.println(a.toString());
-//
-//        int index = StringUtils.lastIndexOf("1313.MP4", ".");
-//        String suffix = StringUtils.substring("1313.MP4", index);
-//        System.out.println(suffix);
-
-//
-//        String str = "邓丽梅|360732199310062621";
-//        String[] identity = str.split("\\|");
-//        System.out.println(identity[0] + " " + identity[1]);
-//
-//
-//        String time = "199509082330";
-//        System.out.println(time.substring(0, 4));
-//        System.out.println(time.substring(4, 6));
-//        System.out.println(time.substring(6, 8));
-//
-//        System.out.println(String.format("%02d", Integer.valueOf("07")));
-//        Object[] elementData  =new Object[10];
-//        int elementCount = 2;
-//        int index = 1;
-//        Object obj = "vector";
-//        Object obj1 = "vector1";
-//
-//        elementData[0] = obj;
-//        elementData[1] = obj;
-//        elementData[2] = obj1;
-//        elementData[3] = obj;
-//        System.arraycopy(elementData, index, elementData, index + 1, elementCount - index);
-//        System.out.print(elementData.toString());
-
-//        System.out.println(System.currentTimeMillis());
-
-
-//        int n = 9 - 1;
-//        n |= n >>> 1;
-//        n |= n >>> 2;
-//        n |= n >>> 4;
-//        n |= n >>> 8;
-//        n |= n >>> 16;
-//
-//        System.out.println(n);
-//
-//        String a = "123";
-//        String b = "123";
-//        System.out.println(a == b);
-
-
-//        String a = new String("100");
-//        String b = new String("100");
-//        Integer c = new Integer(100);
-//        Long d = new Long(100);
-//        System.out.println(a.equals(b));
-//        System.out.println(a.equals(c));
-//        System.out.println(a.equals(d));
-//
-//
-//        char[] charArray = {'a', 'b', 'c'};
-//        char[] charArray1 = charArray;
-//        charArray1[1] = 'x';
-//        System.out.println(charArray);
-
-//        double source = 0.52;
-//        double d1 = Math.round(source * 100) / 100;
-//
-//        double d2 = source * 100;
-//        double d3 = Math.round(d2);
-//        double d4 = d3 / 100;
-//
-//        System.out.println(d1);
-//        System.out.println(d4);
-//
-//
-//        System.out.println(52 / 100);
-//
-//
-//        int i = 1;
-//        int j = 1;
-//        System.out.println(i++ + j);
-//
-//
-//        Double duration = 472.0;
-//        System.out.println(String.format("%.2f", duration / 60));
+        File file = new File("D:\\software\\nginx-1.16.0.zip");
+        String fileMd5 = DigestUtils.md2Hex(new FileInputStream(file));
+        System.out.println(fileMd5);
+        System.out.println(StringUtils.equals("78137943904752ed3c0e8138b8bfd073", md5));
 
     }
 }
