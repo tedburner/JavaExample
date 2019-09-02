bebbc528a00929f94734778bd6677dd3403b5fba
tedburner <jlj_vip98@qq.com>
2019-09-02 11:45:07 +0800

    升级zookeeper版本

---
+++ src/main/java/com/example/algorithm/LeetCode/Code15.java
@@ -0,0 +1,28 @@
+package com.example.algorithm.LeetCode;
+
+import afu.org.checkerframework.checker.igj.qual.I;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2019/9/2 11:32
+ * @description: 三数之和
+ */
+public class Code15 {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> sumArray = new ArrayList<>();
+        List<Integer> array = new ArrayList<>();
+
+        return sumArray;
+
+    }
+}
---
+++ src/main/java/com/example/algorithm/LeetCode/Code8.java
@@ -0,0 +1,50 @@
+package com.example.algorithm.LeetCode;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2019/9/2 10:46
+ * @description: 字符串转换整数 (atoi)
+ */
+public class Code8 {
+
+    public static void main(String[] args) {
+        String str = "2147483648";
+        System.out.println(myAtoi(str));
+    }
+
+    public static int myAtoi(String str) {
+        String character = str.trim();
+        if (character.isEmpty()) {
+            return 0;
+        }
+        char firstChar = character.charAt(0);
+        char flag = '+';
+        int start = 0;
+        if (firstChar == '-' || firstChar == '+') {
+            flag = firstChar;
+            start++;
+        } else if (Character.isDigit(firstChar)) {
+            flag = '+';
+        } else {
+            return 0;
+        }
+        int ans = 0;
+        for (int i = start; i < character.length(); i++) {
+            char c = character.charAt(i);
+            //判断字符串是否是数字
+            if (!Character.isDigit(c)) {
+                break;
+            }
+            int num = c - '0';
+            //需要判断如果加上当前这个数组，是否会越界
+            if ((Integer.MAX_VALUE - num) / 10 < ans) {
+                return flag == '+' ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            ans = ans * 10 + num;
+        }
+        return flag == '+' ? ans : -ans;
+
+    }
+
+
+}
