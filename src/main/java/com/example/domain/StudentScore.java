ceaca1f34145795022f56ee1df8c2d1e72a83923
tedburner <jlj_vip98@qq.com>
2019-10-08 10:02:07 +0800

    Map merge 操作验证

---
+++ src/main/java/com/example/domain/StudentScore.java
@@ -0,0 +1,18 @@
+package com.example.domain;
+
+import lombok.Data;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2019/10/8 09:44
+ * @description:
+ */
+@Data
+public class StudentScore {
+
+    private String stuName;
+
+    private String subject;
+
+    private Integer score;
+}
