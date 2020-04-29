2c27bde42d85a5287da19417246aab5516351a02
lingjun.jlj <jlj_vip98@qq.com>
2020-04-29 16:16:32 +0800

    数组分割

---
+++ src/main/java/com/example/test/ListPageCase.java
@@ -0,0 +1,22 @@
+package com.example.test;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2020/4/29 16:13
+ * @description: Lists.partitions 以固定大小对数组进行切割
+ */
+public class ListPageCase {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        int num = 100;
+        for (int i = 0; i < num; i++) {
+            list.add(i);
+        }
+        System.out.println(Lists.partition(list, 20));
+    }
+}
