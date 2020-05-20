2a942ebcb43f98f790961f9a93b6cc9db48e4608
lingjun.jlj <jlj_vip98@qq.com>
2020-05-20 18:52:34 +0800

    三数相近

---
+++ src/main/java/com/example/algorithm/leetcode/Code18.java
@@ -0,0 +1,21 @@
+package com.example.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2020/5/20 18:51
+ * @description: 四数之和
+ */
+public class Code18 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+
+        return new ArrayList<>();
+    }
+}
--- src/main/java/com/example/algorithm/leetcode/Code6.java
+++ src/main/java/com/example/algorithm/leetcode/Code6.java
@@ -1,5 +1,8 @@
 package com.example.algorithm.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author: lingjun.jlj
  * @date: 2019/8/13 14:35
@@ -8,22 +11,48 @@ package com.example.algorithm.leetcode;
 public class Code6 {
 
     public static void main(String[] args) {
-
+        System.out.println(convert("AB", 1));
     }
 
+    /**
+     * 字形变换，通过设置flag标志位，来表示Z字形的跳转位置
+     *
+     * @param s       指定字符串
+     * @param numRows Z字形行数
+     * @return
+     */
     public static String convert(String s, int numRows) {
-        if (s.isEmpty() || s.length() == 1) {
+    /*
+    0       4       8
+    1   3   5   7   9
+    2       6       10
+
+    0           6           12
+    1       5   7       11  13
+    2   4       8   10      14
+    3           9           15
+ */
+        if (numRows < 2) {
             return s;
         }
-        int len = s.length();
+        List<StringBuilder> list = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) list.add(new StringBuilder());
+        int flag = -1;
         int index = 0;
-        Character[][] dp = new Character[len][4];
-        for (int k = 0; k < numRows; k++) {
-            for (int i = 0; i < len; i++) {
-                char ch = s.charAt(index);
+        for (int k = 0; k < s.length(); k++) {
+            char ch = s.charAt(k);
+            list.get(index).append(ch);
+            //判断Z字形跳转处
+            if (index == 0 || index == (numRows - 1)) {
+                flag = -flag;
             }
+            index += flag;
         }
-        return "";
+        StringBuilder ans = new StringBuilder();
+        for (StringBuilder builder : list) {
+            ans.append(builder);
+        }
+        return ans.toString();
     }
 
 }
