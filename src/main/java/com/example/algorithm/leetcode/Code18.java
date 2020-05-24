83a0247fead933a87a778af67292a2c7eb2dba1d
tedburner <jlj_vip98@qq.com>
2020-05-24 21:48:14 +0800

    四数和

--- src/main/java/com/example/algorithm/leetcode/Code18.java
+++ src/main/java/com/example/algorithm/leetcode/Code18.java
@@ -1,6 +1,7 @@
 package com.example.algorithm.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -11,11 +12,38 @@ import java.util.List;
 public class Code18 {
 
     public static void main(String[] args) {
-
+        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
+        //int[] nums = new int[]{1, 0, -1, 0, -2, 2, 0};
+        System.out.println(fourSum(nums, -1));
     }
 
     public static List<List<Integer>> fourSum(int[] nums, int target) {
-
-        return new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int k = i + 1; k < len; k++) {
+                if (k > i + 1 && nums[k] == nums[k - 1]) continue;
+                int left = k + 1, right = len - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[k] + nums[left] + nums[right];
+                    if (sum == target) {
+                        ans.add(Arrays.asList(nums[i], nums[k], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    }
+                    if (sum < target) {
+                        left++;
+                    }
+                    if (sum > target) {
+                        right--;
+                    }
+                }
+            }
+        }
+        return ans;
     }
 }
