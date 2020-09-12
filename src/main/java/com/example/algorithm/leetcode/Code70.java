3a461f5c44a3121bc0879e6a619aef5bd72c9199
tedburner <jlj_vip98@qq.com>
2020-09-12 17:16:32 +0800

    leetcode

--- src/main/java/com/example/algorithm/leetcode/Code70.java
+++ src/main/java/com/example/algorithm/leetcode/Code70.java
@@ -9,11 +9,23 @@ public class Code70 {
 
     public static void main(String[] args) {
         Code70 code = new Code70();
-        System.out.println(code.climbStairs(0));
+        long start = System.currentTimeMillis();
+        System.out.println(code.climbStairs(5));
+        System.out.println(code.climb(5));
+        System.out.println("耗时：" + (System.currentTimeMillis() - start) + "ms");
     }
 
     public int climbStairs(int n) {
-        return climb(0, n);
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n - 1];
     }
 
     /**
@@ -28,5 +40,25 @@ public class Code70 {
             return 1;
         }
         return climb(i + 1, n) + climb(i + 2, n);
+    }
+
+    /**
+     * 递归暴力破解
+     * 时间超时
+     */
+    public int climb(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (i <= 1) {
+                dp[i] = i + 1;
+            }
+            if (i > 1) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+        }
+        return dp[n - 1];
     }
 }
---
+++ src/main/java/com/example/algorithm/leetcode/Code83.java
@@ -0,0 +1,43 @@
+package com.example.algorithm.leetcode;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2020/9/12 15:23
+ * @description: 删除排序链表中的重复元素
+ * 思路：因为是有序列表，所以比较相邻的节点，如果下个节点和当前节点相同，就删除下个节点
+ */
+public class Code83 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode node = head;
+        while (node != null && node.next != null){
+            //判断当前链表和下一个节点是否相同
+            if (node.val == node.next.val){
+                node.next = node.next.next;
+            }else {
+                node = node.next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+        ListNode result = deleteDuplicates(head);
+        System.out.println(result);
+
+    }
+}
