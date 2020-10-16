39eb6ea83f78ede38b60bfbc8f197b2cc6c8d45d
lingjun.jlj <jlj_vip98@qq.com>
2020-10-16 17:43:33 +0800

    搜索插入位置

--- src/main/java/com/example/algorithm/leetcode/Code35.java
+++ src/main/java/com/example/algorithm/leetcode/Code35.java
@@ -3,36 +3,33 @@ package com.example.algorithm.leetcode;
 /**
  * @author: lingjun.jlj
  * @date: 2019/9/9 14:39
- * @description: 搜索插入位置，以及排好序的数组，使用二分查找
+ * @description: 搜索插入位置
  */
 public class Code35 {
 
     public static void main(String[] args) {
-        int[] nums = {1, 3, 5, 6};
-        int target = 2;
+        int[] nums = {1};
+        int target = 1;
         System.out.println(searchInsert(nums, target));
     }
 
     public static int searchInsert(int[] nums, int target) {
-        int len = nums.length;
-        if (nums[len - 1] < target) {
-            return len;
-        }
-        return binarySearch(nums, target, 0, nums.length);
-    }
-
-    public static int binarySearch(int[] nums, int target, int left, int right) {
-        if (left > right) {
+        int length = nums.length;
+        if (length == 0) {
             return 0;
         }
-        int mid = (left + right) / 2;
-        int len = nums.length;
-        if (nums[mid] > target) {
-            return binarySearch(nums, target, left, mid - 1);
-        } else if (nums[mid] < target) {
-            return binarySearch(nums, target, mid + 1, right);
+        if (nums[0] >= target) {
+            return 0;
         }
-        return mid;
+        for (int i = 1; i < length; i++) {
+            if (nums[i - 1] < target && nums[i] >= target) {
+                return i;
+            }
+        }
+        if (nums[length - 1] < target) {
+            return length;
+        }
+        return length;
     }
 
 }
