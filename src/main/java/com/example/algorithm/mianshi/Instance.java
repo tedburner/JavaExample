19304a04dd08e9d7239dd65deef8fe83848683d1
tedburner <jlj_vip98@qq.com>
2020-10-24 23:18:31 +0800

    单例

---
+++ src/main/java/com/example/algorithm/mianshi/Instance.java
@@ -0,0 +1,12 @@
+package com.example.algorithm.mianshi;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2020/10/22 21:14
+ * @description: 单例
+ */
+public class Instance {
+
+    private static volatile Instance  instance = null;
+
+}
