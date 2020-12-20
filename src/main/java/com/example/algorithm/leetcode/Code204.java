b629857a6608905cee81ec2a1f1e3dd24912db0e
tedburner <jlj_vip98@qq.com>
2020-12-20 23:26:12 +0800

    计数质数

---
+++ src/main/java/com/example/algorithm/leetcode/Code204.java
@@ -0,0 +1,37 @@
+package com.example.algorithm.leetcode;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2020/12/20 22:59
+ * @description: 计数质数
+ */
+public class Code204 {
+
+    public int countPrimes(int n) {
+        if (n == 0 || n == 1) {
+            return 0;
+        }
+        int sum = 0;
+        for (int i = 2; i < n; i++) {
+            //判断是否是质数
+            boolean isPrimes = isPrimes(i);
+            sum += isPrimes ? 1 : 0;
+        }
+        return sum;
+    }
+
+    public boolean isPrimes(int x) {
+        for (int i = 2; i * i <=x; i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        System.out.println(new Code204().countPrimes(10));
+        System.out.println("耗时：" + (System.currentTimeMillis() - startTime) + "ms");
+    }
+}
